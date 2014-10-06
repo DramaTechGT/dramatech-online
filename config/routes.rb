8d890dcfe10618fd0a978fa96000285eec01e3d5
Dean Papastrat <dean.g.papastrat@gmail.com>
2014-10-05 22:43:21 -0400

    Prepare devise for later use

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # Development Engines
+  mount Nkss::Engine => '/styleguides' if Rails.env.development?
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   devise_for :users
   resources :posts
